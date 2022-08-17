473164fcd87fbf21fc01316a694a3d6b6e1c3008
valeryO <olennval@mail.ru>
2022-08-18 00:03:41 +0700

    number 25

---
+++ N25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите два натуральных числа: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int prois = 1;
+for (int i = 0; i < B; i++)
+{
+    prois *= A;
+}
+Console.WriteLine($"{A}^{B} = {prois}");
