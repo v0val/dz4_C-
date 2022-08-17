7933d9b028edd021d49a512dcf9664e6a80ff5e7
valeryO <olennval@mail.ru>
2022-08-18 00:16:22 +0700

    number 27

---
+++ N27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int reserv = A;
+int sum = 0;
+while (A > 0)
+{
+    sum += A % 10;
+    A /= 10;
+}
+Console.WriteLine($"Сумма цифр числа {reserv} = {sum}");
